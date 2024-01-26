ec4ee45af0d19c967daaab8e2cd52c5a5a989b10
Igor Honhoff <igor@flarehub.io>
2024-01-26 14:25:50 +0200

    Update pluggable-electron and electron versions

--- main.js
+++ main.js
@@ -4,59 +4,57 @@ const path = require('path')
 const pe = require('pluggable-electron/main')
 
 function createWindow() {
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 1000,
+		height: 800,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			sandbox: false,
+		},
+	})
 
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+	// and load the index.html of the app.
+	mainWindow.loadFile('./index.html')
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('./index.html')
-
-  // Open the DevTools.
-  if (!app.isPackaged) mainWindow.webContents.openDevTools()
+	// Open the DevTools.
+	if (!app.isPackaged) mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  //Initialise pluggable Electron
-  pe.init(
-    {
-      // Function to check from the main process that user wants to install a plugin
-      confirmInstall: async plugins => {
-        const answer = await dialog.showMessageBox({
-          message: `Are you sure you want to install the plugin ${plugins.join(', ')}`,
-          buttons: ['Ok', 'Cancel'],
-          cancelId: 1,
-        })
-        console.log('Main:', answer);
-        return answer.response == 0
-      },
-      // Path to install plugin to
-      pluginsPath: path.join(app.getPath('userData'), 'plugins')
-    }
-  )
+	//Initialise pluggable Electron
+	pe.init({
+		// Function to check from the main process that user wants to install a plugin
+		confirmInstall: async plugins => {
+			const answer = await dialog.showMessageBox({
+				message: `Are you sure you want to install the plugin ${plugins.join(', ')}`,
+				buttons: ['Ok', 'Cancel'],
+				cancelId: 1,
+			})
+			console.log('Main:', answer)
+			return answer.response == 0
+		},
+		// Path to install plugin to
+		pluginsPath: path.join(app.getPath('userData'), 'plugins'),
+	})
 
-  createWindow()
+	createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+	app.on('activate', function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
